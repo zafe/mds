ca730e42c6cb3354d2585fffcf18f494a3dfdf32
correag <correag@bbabf034-7b44-0410-b41f-e15aafb91961>
2015-10-22 13:42:54 +0000

    modelo Expediente
    git-svn-id: http://10.10.0.207/svn/mds/php/expedientes/trunk@4811 bbabf034-7b44-0410-b41f-e15aafb91961

--- app/Expediente.php
+++ app/Expediente.php
@@ -9,19 +9,19 @@ class Expediente extends Model
     protected $table='expediente';
 
     public static function buscar($num,$cod,$anio){
-      return \DB::select('
-      select *
-      from expediente
-      where
-          expcod = :codigo
-          and  expanio = :anio
-          and  expnum = :numero
-      ',
-          [
-            'codigo' => $cod,
-            'anio' => $anio,
-            'numero' => $num
-          ]
-      );
+      $parametros=[
+        'codigo' => $cod,
+        'anio' => $anio,
+        'numero' => $num
+        ];
+      $sql='
+        select *
+        from expediente
+        where
+            expcod = :codigo
+            and  expanio = :anio
+            and  expnum = :numero
+        ';
+      return \DB::select($sql,$parametros);
     }
 }
